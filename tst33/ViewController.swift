238ac1102d93904cf4dac84d0f90189dde964b2f
Egopoler <egpol2004@yandex.ru>
2023-06-26 23:10:29 +0300

    HW

---
+++ tst33/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  tst33
+//
+//  Created by MacBook Pro on 26.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+ 
+    @IBOutlet weak var textInf1: UITextView!
+    @IBOutlet weak var LineUnderName: UIView!
+    @IBOutlet weak var BigFigure: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func but1Trapped(_ sender: UIButton) {
+        
+    }
+    
+}
+
